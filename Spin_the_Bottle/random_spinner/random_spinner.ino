1e1dceb3f933b047cd50d9b3d6869a71d41d476a
samiosborn <samiosborn@live.co.uk>
2025-08-19 18:17:54 +0100

    Bottle spinner constants

---
+++ Spin_the_Bottle/random_spinner/random_spinner.ino
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+// pin for the switch (input)
+const uint8_t SWITCH_PIN = 3;
+
+// pin for the motor PWM (output)
+const uint8_t MOTOR_PWM_PIN = 5;
+
+// Duty cycle of 80% (0–255)
+const uint8_t DUTY_80PC = 204;
+
+// Motor on time in milliseconds
+const unsigned long ON_MS = 2000;
+
+// Debounce time in milliseconds
+const unsigned long DEBOUNCE_MS = 25;
+
+void setup() {
+
+}
+
+void loop() {
+
+}
