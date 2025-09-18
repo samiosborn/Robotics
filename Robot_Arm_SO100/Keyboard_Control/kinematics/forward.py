121319a3541e028bdbd158fdaccfaa36832c5b8d
samiosborn <samiosborn@live.co.uk>
2025-09-18 21:26:12 +0100

    FK and Jacobian

---
+++ Robot_Arm_SO100/Keyboard_Control/kinematics/forward.py
@@ -0,0 +1,45 @@
+# forward.py
+import numpy as np
+from typing import List, Tuple
+import config
+from .dh import mdh
+from .pose import R_to_rpy, rpy_to_R
+
+# Build frames along kinematic chain
+def fk_all_frames(q_rad: List[float]) -> List[np.ndarray]:
+    # Initialise transformation matrix
+    Ts = [np.eye(4)]
+    for i, (a, alpha, d, theta0) in enumerate(config.MDH_PARAMS):
+        # Add offset to commanded angle
+        theta = theta0 + float(q_rad[i])
+        # Build joint transformation
+        Ti = mdh(a, alpha, d, theta)
+        # Chain to previous frame
+        Ts.append(Ts[-1] @ Ti)
+    # Tool orientation
+    roll = np.deg2rad(config.TOOL_RPY_DEG[0])
+    pitch = np.deg2rad(config.TOOL_RPY_DEG[1])
+    yaw = np.deg2rad(config.TOOL_RPY_DEG[2])
+    # Convert tool orientation to 3x3 rotation matrix
+    R_tool = rpy_to_R(roll, pitch, yaw)
+    # Tool position offset
+    p_tool = np.array(config.TOOL_POS_M, dtype=float).reshape(3,)
+    # Single 4x4 homogeneous transform
+    T_tool = np.eye(4)
+    T_tool[:3,:3] = R_tool
+    T_tool[:3, 3] = p_tool
+    # End effector frame appended
+    Ts.append(Ts[-1] @ T_tool)
+    return Ts
+
+# End effector pose as (pos xyz, rpy)
+def fk_pose6(q_rad: List[float]) -> Tuple[np.ndarray, np.ndarray]:
+    # Extract tool frame
+    T = fk_all_frames(q_rad)[-1]
+    # Position
+    p = T[:3,3]
+    # Rotation matrix
+    R = T[:3,:3]
+    # Vector (roll, pitch and yaw) fromm rotation matrix
+    rpy = np.array(R_to_rpy(R))
+    return p, rpy
---
+++ Robot_Arm_SO100/Keyboard_Control/kinematics/jacobian.py
@@ -0,0 +1,27 @@
+# jacobian.py
+import numpy as np
+from typing import List
+from .forward import fk_all_frames
+
+# Geometric Jacobian matrix: J = [ Jv ; Jr ]
+def geometric_jacobian(q_rad: List[float]) -> np.ndarray:
+    # Compute all frames along the kinematic chain
+    Ts = fk_all_frames(q_rad)
+    # Position of end effector (in base coordinates)
+    p_e = Ts[-1][:3,3]
+    # Initialise linear component
+    Jv = np.zeros((3,6), dtype=float)
+    # Initialise angular component
+    Jr = np.zeros((3,6), dtype=float)
+    for i in range(1, 7):
+        # Transformation of frame {i-1} (in base frame)
+        Ti_1 = Ts[i-1]
+        # Extract Z-axis (in base coordinates)
+        z = Ti_1[:3,2]
+        # Position of origin 
+        p = Ti_1[:3,3]
+        # Linear velocity part: Jv_i = z_(i-1) x (p_e - p_(i-1))
+        Jv[:, i-1] = np.cross(z, (p_e - p))
+        # Revolute portion: Jr_i = z_(i-1)
+        Jr[:, i-1] = z
+    return np.vstack([Jv, Jr])
