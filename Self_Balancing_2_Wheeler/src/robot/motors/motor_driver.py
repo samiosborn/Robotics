58c3e282d1b5c8136f5e1f7406ea229acf9427d2
Sami Osborn <samiosborn@live.co.uk>
2025-11-26 20:07:27 +0000

    Set duty

--- Self_Balancing_2_Wheeler/src/robot/motors/motor_driver.py
+++ Self_Balancing_2_Wheeler/src/robot/motors/motor_driver.py
@@ -33,3 +33,34 @@ class MotorDriver:
         # Enable H-bridge outputs
         self._scmd.enable()
     
+    # --- Private Methods --- 
+    # Encode float duty command
+    def _encode_level(self, duty):
+        # Stop
+        if duty == 0:
+            return 1, 0
+        # Clip
+        duty = max(-1.0, min(1.0, duty))
+        # Determine direction
+        direction = 1 if duty >= 0 else 0
+        # Scale magniture to -255 to 255
+        level = int(duty * 255)
+        return direction, level
+    # Write motor command
+    def _write_motor(self, motor_id, duty):
+        # Encode level
+        direction, level = self._encode_level(duty)
+        # Set drive
+        self._scmd.set_drive(motor_id, direction, level)
+    
+    # --- Public API ---
+    # Set duty command
+    def set_duty(self, left_duty, right_duty):
+        # Write motors
+        self._write_motor(self._left_motor_id, left_duty)
+        self._write_motor(self._right_motor_id, right_duty)
+    # Stop command
+    def stop(self):
+        # Stop motors
+        self._write_motor(self._left_motor_id, 0)
+        self._write_motor(self._right_motor_id, 0)
