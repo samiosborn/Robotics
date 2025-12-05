5cd5458452ad7a5dddc2ee202376ca85c6f58950
Sami Osborn <samiosborn@live.co.uk>
2025-12-05 17:00:20 +0000

    Exponential and log mapping

--- MonoSLAM/src/utils/quaternion.py
+++ MonoSLAM/src/utils/quaternion.py
@@ -32,3 +32,71 @@ def quat_conjugate(q):
   q_w, q_x, q_y, q_z = q
   # Negate the vector components
   return np.array([q_w, -q_x, -q_y, -q_z])
+
+# Quaternion to rotation matrix
+def quat_to_rotmat(q):
+  # Normalise q
+  q = normalise(q)
+  # Unpack
+  q_w, q_x, q_y, q_z = q
+  # Polynomial formula
+  return np.array([
+    [1-2*(q_y**2+q_z**2), 2*(q_x*q_y-q_z*q_w), 2*(q_x*q_z+q_y*q_w)],
+    [2*(q_x*q_y+q_z*q_w), 1-2*(q_x**2+q_z**2), 2*(q_y*q_z-q_x*q_w)],
+    [2*(q_x*q_z-q_y*q_w), 2*(q_y*q_z+q_x*q_w), 1-2*(q_x**2+q_y**2)]
+  ])
+
+# Rotation vector to quaternion over timestep (exponential map)
+def rotvec_to_quat(omega, dt):
+  # Rotation vector for timestep
+  phi = omega * dt
+  # Angle
+  theta = np.linalg.norm(phi)
+  if theta < 1e-8:
+      # Small angle approximation
+      w = 1 - 0.125 * theta**2
+      v = 0.5 * phi
+  else:
+      # Axis
+      axis = phi / theta
+      # Exact angle-axis method
+      w = np.cos(theta/2)
+      v = axis * np.sin(theta/2)
+  return np.concatenate([[w], v])
+
+# Quaternion from axis-angle representation
+def quat_from_axis_angle(axis, theta):
+  # Combine
+  q = np.concatenate([[np.cos(theta/2)], (np.sin(theta/2) * axis)])
+  return normalise(q)
+
+# Integrate quaternion over timestep
+def integrate_quat(q, omega, dt):
+  # Quaternion increment
+  dq = rotvec_to_quat(omega, dt)
+  # Multiply quaternions
+  q_new = quat_mul(dq, q)
+  # Normalise
+  return normalise(q_new)
+
+# Quaternion log (log mapping)
+def quat_log(q):
+  # Normalise
+  q = normalise(q)
+  # Unpack
+  q_w, q_x, q_y, q_z = q
+  # Pack
+  v = np.array([q_x, q_y, q_z])
+  # Sin(theta/2)
+  sin_half_theta = np.linalg.norm(v)
+  if sin_half_theta < 1e-8:
+    # Small-angle approximation
+    return 2 * v
+  else:
+    # Theta
+    theta = 2 * np.arccos(q_w)
+    # Axis
+    axis = v / sin_half_theta
+    # Axis-angle
+    phi = theta * axis
+    return phi
