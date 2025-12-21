6e3120d8d1ff6ef59f78489cff644fc5b6e7a725
Sami Osborn <samiosborn@live.co.uk>
2025-12-21 14:30:06 +0000

    Triangulation

---
+++ MonoSLAM/src/geometry/triangulation.py
@@ -0,0 +1,15 @@
+# src/geometry/triangulation.py
+import numpy as np
+from geometry.homogeneous import homogenise, dehomogenise
+
+# Triangulate a point
+def triangulate_point():
+
+# Check if point is in front of camera
+def point_infront_camera():
+
+# Cheirality check for two views
+def cheirality_check():
+
+# Select the valid pose from candidates
+def select_valid_pose(): 
