a08cc1102a1404a7f24c4c0512c9b292e45cd14a
Sami Osborn <samiosborn@live.co.uk>
2025-12-26 14:14:21 +0000

    Algebraic residual (vectorised)

--- MonoSLAM/src/geometry/epipolar.py
+++ MonoSLAM/src/geometry/epipolar.py
@@ -2,27 +2,25 @@
 import numpy as np
 from geometry.homogeneous import homogenise
 
-# Epipolar constraint
-def epipolar_constraint(x1, x2, F): 
-    # x2^T F x1 = 0
-    return float(x2.T @ F @ x1)
-
-# Normalised epipolar constraint
-def normalised_epipolar_constraint(x1_hat, x2_hat, E): 
-    # x2_hat^T E x1_hat = 0
-    return float(x2_hat.T @ E @ x1_hat)
-
-# Mean epipolar residual
-def mean_epipolar_residual(x1, x2, F):
+# Algebraic residual
+def algebraic_residual(x1, x2, F):
+    # Assert
+    x1 = np.asarray(x1, dtype=float)
+    x2 = np.asarray(x2, dtype=float)
+    # Shape error
+    if x1.shape[0] != 2:
+        raise ValueError(f"Shape should be (2, N)")
     # Homogenise
     x1h = homogenise(x1)
     x2h = homogenise(x2)
-
     # Epipolar lines in image 2
     Fx1 = F @ x1h
-
     # Residual r_i = x2_i^T F x1_i
-    r = np.sum(x2h * Fx1, axis=0)
+    return np.sum(x2h * Fx1, axis=0)
 
+# Algebratic residual RMSE
+def algebraic_residual_RMSE(x1, x2, F):
+    # Algebraic residuals
+    r = algebraic_residual(x1, x2, F)
     # Root mean squared residual
     return np.sqrt(np.mean(r ** 2))
