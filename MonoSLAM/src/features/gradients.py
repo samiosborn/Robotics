5622bf019fd3a14a157ab51eca6c299144877377
Sami Osborn <samiosborn@live.co.uk>
2026-02-03 21:12:39 +0000

    PIL to NumPy

--- MonoSLAM/src/features/gradients.py
+++ MonoSLAM/src/features/gradients.py
@@ -2,42 +2,45 @@
 import numpy as np
 from PIL import Image
 
-# Compute image gradients
-def compute_image_gradients(img, cfg):
-    # Unpack cfg
-    # Convert PIL input to greyscale
-    # Implement padding
-    # Calc gradients with magnitude and orientation
-    # Return an output dict (Ix, Iy, mag, ori)
-    raise NotImplementedError
-
 # Image to greyscale
 def img_to_grey(img, luminance_weights, assume_srgb=True, normalise_01=True, dtype=np.float64):
-    # If PIL.Image (mode either L, I;16, RGB, RGBA) 
-    # Convert to RGB (if colour)
-    # Convert to NumPy array (if colour: (H, W, 3) shape)
-    # Convert to float64 in [0,1]
-    # Linearise each channel (inverse gamma)
-    # Convert to greyscale using weights (dot product)
-    # Return 2D array (H, W)
+    # If PIL.Image 
+    if isinstance(img, Image.Image): 
+        if img.mode == "L": 
+            # Convert to NumPy array (H, W)
+        elif img.mode == "I;16":
+            # Convert to NumPy array (H, W) 
+        elif img.mode == "RGB": 
+            # Convert to NumPy array (H, W, 3) 
+        elif img.mode == "RGBA": 
+            # Convert to RGB (drop alpha (A))
+            # Convert to NumPy array (H, W, 3) 
+        else: 
+            # Not accepted PIL.Image mode 
+            raise ValueError("Image: Only accepts PIL.Image as modes L, I;16, RGB, or RGBA")
+    # If NumPy array or PIL.Image converted to NumPy
+    if isinstance(img, np.ndarray): 
+        # Validate shape (if RGBA, drop A)
+        # Normalise to [0,1]
+        # Inverse gamma
+        # If 3D, convert to greyscale 
+        # Cast to float64
+        # Return 2D array (H, W)
     raise NotImplementedError
 
-# Convert to RGB
-def img_to_rgb(img, dtype=np.float64): 
-    # If NumPy: require (H,W,3) or (H,W,4) and raise otherwise
-    # If PIL: convert mode to "RGB"
-    raise NotImplementedError
-
-# Inverse gamma
-def inv_gamma(img, dtype=np.float64): 
-    # Input: sRGB in [0, 1]
-    # Output: linear RGB in [0,1]
-    raise NotImplementedError
+    else: 
+        # Not NumPy or accepted PIL.Image
+        raise ValueError("Image: Only accepts PIL.Image or NumPy as array")
 
 # Normalise to unit interval [0, 1]
 def to_unit_interval(img, dtype=np.float64, eps=1e-8): 
     # For uint8, uint16: divide by max representable
     # For floats: clip to [0,1] (allow tiny tolerance) - raise error otherwise
+    raise NotImplementedError
+
+# Inverse gamma 
+def inv_gamma(img, dtype=np.float64): 
+    # Per channel inverse gamma 
     raise NotImplementedError
 
 # Gaussian kernel 1D
@@ -99,7 +102,7 @@ def gradients_sobel(im, sigma_d, sobel_ksize=3, sobel_scale=0.125, truncate=3.0,
     raise NotImplementedError
 
 # Magnitude of gradient
-def gradient_magnitude(Ix, Iy, eps=1e-8:
+def gradient_magnitude(Ix, Iy, eps=1e-8):
     # Compute mag = sqrt(Ix^2 + Iy^2)
     # Return mag
     raise NotImplementedError
@@ -108,4 +111,13 @@ def gradient_magnitude(Ix, Iy, eps=1e-8:
 def gradient_orientation(Ix, Iy):
     # Compute ori = arctan2(Iy, Ix) in radians (range [-pi, pi])
     # Return ori
+    raise NotImplementedError
+
+# Compute image gradients
+def compute_image_gradients(img, cfg):
+    # Unpack cfg
+    # Convert PIL input to greyscale
+    # Implement padding
+    # Calc gradients with magnitude and orientation
+    # Return an output dict (Ix, Iy, mag, ori)
     raise NotImplementedError
